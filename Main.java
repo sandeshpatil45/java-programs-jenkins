e5691617f03baacb62d4b33bcb76c586e75349a0
sandeshpatil45 <patilsj2023@gmail.com>
2025-04-26 14:18:22 +0530

    jenkins demo1

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.sql.Date;
+
+public class Main{
+  public static void main(String args[]){
+         System.out.println("Progrsm of java for jenkins");
+         System.out.println(new Date(0));
+
+  }    
+}
